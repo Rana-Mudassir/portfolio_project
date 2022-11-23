8e05a37f3bdb39c07536743c153d7bd65b79ccc4
Rana-Mudassir <mduasire38@gmail.com>
2022-11-23 22:09:51 +0500

    Update linters erorrs

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let btn = document.querySelector('.main_btn');
-btn.addEventListener('click', on);
+const btn = document.querySelector('.main_btn');
 
 function on() {
   document.getElementById('layer').style.display = 'block';
 }
+btn.addEventListener('click', on);
+
+const btn2 = document.querySelector('style-li');
 
 function off() {
   document.getElementById('layer').style.display = 'none';
 }
+
+btn2.addEventListener('click', off);
