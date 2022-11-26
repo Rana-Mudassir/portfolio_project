025fedd944b941f3a461775be2b40722efa1c924
Rana-Mudassir <mduasire38@gmail.com>
2022-11-26 13:25:14 +0500

    Removed Errors

--- script.js
+++ script.js
@@ -79,6 +79,8 @@ const cardsObj = [
   },
 ];
 
+// eslint-disable no-return-assign 
+
 cardsObj.map(
   (card, value) => (cardSection.innerHTML += `<div class="card  rev-${value}">
         <div class="card-img">
